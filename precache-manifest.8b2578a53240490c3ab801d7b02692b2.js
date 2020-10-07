1fc855b405db7e22f25699778692734d09a0ba41
Prosperity <prosperauthor@gmail.com>
2020-10-07 21:09:20 +0100

    Updates

---
+++ precache-manifest.8b2578a53240490c3ab801d7b02692b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "860be8726377451619d1",
+    "url": "/TokenFarm/static/css/main.9e7a6e38.chunk.css"
+  },
+  {
+    "revision": "860be8726377451619d1",
+    "url": "/TokenFarm/static/js/main.860be872.chunk.js"
+  },
+  {
+    "revision": "925337cdf0971f768773",
+    "url": "/TokenFarm/static/css/1.35a88edb.chunk.css"
+  },
+  {
+    "revision": "925337cdf0971f768773",
+    "url": "/TokenFarm/static/js/1.925337cd.chunk.js"
+  },
+  {
+    "revision": "bad26f9a1b614e313c81",
+    "url": "/TokenFarm/static/js/runtime~main.bad26f9a.js"
+  },
+  {
+    "revision": "1446008038a16f77df70458c3d27a8db",
+    "url": "/TokenFarm/index.html"
+  }
+];
